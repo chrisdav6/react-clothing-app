19be550fdce72449eb1c676cead972ac1dd2f24d
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-10-23 14:18:07 -0400

    Created Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './header/Header';
 import Homepage from '../pages/homepage/Homepage';
 import Shop from '../pages/shop/Shop';
 import './app.scss';
@@ -7,6 +8,7 @@ import './app.scss';
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route path='/' exact component={Homepage} />
         <Route path='/shop' component={Shop} />
