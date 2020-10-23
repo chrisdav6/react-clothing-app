57b4086989655b2cce6e8595fbdeeb153a2927fd
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-10-23 11:21:51 -0400

    Setup react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from '../pages/homepage/Homepage';
 import './app.scss';
 
 const App = () => {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route path='/' exact component={Homepage} />
+      </Switch>
     </div>
   );
 };
