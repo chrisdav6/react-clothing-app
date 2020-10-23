ae1976a9d22ce698fc56028965df6a98c150a227
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-10-23 12:44:07 -0400

    Update MenuItem to navigate withRouter

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -4,27 +4,44 @@ import './directory.scss';
 
 const Directory = () => {
   const [sections, setSections] = useState([
-    { title: 'Hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png', id: 1 },
-    { title: 'Jackets', imageUrl: 'https://i.ibb.co/px2tCc3/hats.png', id: 2 },
-    { title: 'Sneakers', imageUrl: 'https://i.ibb.co/0jqHpnp/hats.png', id: 3 },
+    {
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      id: 1,
+      linkUrl: 'hats'
+    },
+    {
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/hats.png',
+      id: 2,
+      linkUrl: 'jackets'
+    },
+    {
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/hats.png',
+      id: 3,
+      linkUrl: 'sneakers'
+    },
     {
       title: 'Womens',
       imageUrl: 'https://i.ibb.co/GCCdy8t/hats.png',
       id: 4,
+      linkUrl: 'womens',
       size: 'large'
     },
     {
       title: 'Mens',
       imageUrl: 'https://i.ibb.co/R70vBrQ/hats.png',
       id: 5,
+      linkUrl: 'mens',
       size: 'large'
     }
   ]);
 
   return (
     <div className='directory-menu'>
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
   );
